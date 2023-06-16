b3ff73361703a507a8fc1b0a2482f88fc4a73c30
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-06-16 08:42:06 +0530

    some file and folder added

--- crowdFunding/client/src/App.jsx
+++ crowdFunding/client/src/App.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from "react";
+import './index.css';
 
 const App = () => {
-  return <div><h1>Kunal </h1></div>
+  return (
+    <div>
+      <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row text-white">
+        <div className="sm:flex hidden mr-10 relative">
+            sidebar
+        </div>
 
-} 
+        <div className="flex-1 max-sw:w-full max-w-[1280px] mx-auto sm:pr-5">
+            navbar
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- crowdFunding/client/src/main.jsx
+++ crowdFunding/client/src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { ReactDOM } from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import {BrowserRouter as Router} from "react-router-dom";
 import{ThirdwebProvider,ChainId} from "@thirdweb-dev/react";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 
 root.render(
     <ThirdwebProvider>
